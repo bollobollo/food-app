80fec2e7eac2d61230e165eb275eb62ed87edf1b
bollo <maardeva@gmail.com>
2023-01-15 17:00:26 -0600

    commit 4

--- src/home-illustration.js
+++ src/home-illustration.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 const Main = styled.div`
-    width: 1312px;
-    height: 928px;
     display: flex;
     flex-flow: column wrap;
+    width: 1312px;
+    height: 928px;
     border: 25px black solid;`;
 
 
 const Frame1 = styled.div`
     width: 640px;
     height: 928px;
+    flex: 2 1 auto;
     background-color: #3d3d3d;
     margin-right: 32px; 
     `;
@@ -18,12 +19,14 @@ const Frame1 = styled.div`
 const Frame2 = styled.div`
     width: 640px;
     height: 544px;
+    flex: 1 1 auto;
     background-color: #D9D9D9; 
     `;
 
 const Frame3 = styled.div`
     width: 640px;
     height: 352px;
+    flex: 1 1 auto;
     background-color: #D0D0D0;
     margin-top: 32px;
     `;
