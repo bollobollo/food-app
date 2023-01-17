1bfc9e6acb34de33349288059234a6a916049876
bollo <maardeva@gmail.com>
2023-01-17 17:00:19 -0600

    commit 7

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
 import './App.css';
-import Ill from './home-illustration.js'
+// import Ill from './home-illustration.js'
 import styled, {css} from "styled-components";
 
 const Wrapper = styled.div`
+  // Dimensiones de la caja
   height: 100vh;
-  width: 1fr;
+  width: 100vw;
+  max-width: 2000px;
+  min-width: 800px;
+  // Definición de rejilla y dimensiones de retícula
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(8, 1fr);
+  gap: 1em;
   column-gap: 1em;
   row-gap: 1em;
+  // Estilos
   background: green;
   `;
 
 const Box = styled.div`
   background: gray;
   border-radius: 2vw;
-  grid-column-start: 1;
-  grid-column-end: 2;
-  grid-row-start: 1;
-  grid-row-end: 9;
+  word-wrap: break-word;
+  //Posicionamiento por línea
+  grid-column: 1;
+  grid-row: 1 / 9;
 
   ${props => props.two && css`
     background: palevioletred;
     color: black;
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 6;
+    //Posicionamiento por línea
+    grid-column: 2 / 3;
+    grid-row: 1 / 6;
   `}
 
   ${props => props.three && css`
     background: yellow;
     color: black;
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 6;
-    grid-row-end: 9;
+    //Posicionamiento por línea
+    grid-column: 2 / 3;
+    grid-row: 6 / 9;
 
     ${props => props.wide && css`
       color: green;
@@ -44,13 +48,23 @@ const Box = styled.div`
   `}
 `;
 
+const Text = styled.p`
+  width: 250px;
+  height: 500px;
+  background: gray;
+  color: black;
+`;
+
 function App() {
   return (
-    <Wrapper className="App">
-      <Box>Hola</Box>
-      <Box two>Hola</Box>
-      <Box three wide>Hola</Box>
-    </Wrapper>
+    <div>
+      <Wrapper className="App">
+        <Box>Hola</Box>
+        <Box two>Hola</Box>
+        <Box three wide>Hola</Box>
+      </Wrapper>
+    </div>
+
   );
 }
 
