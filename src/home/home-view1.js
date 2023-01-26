21ce74fe7c684d484ec47a9e9d469883e8a75dc3
bollo <maardeva@gmail.com>
2023-01-26 16:53:21 -0600

    commit 14

--- src/home/home-view1.js
+++ src/home/home-view1.js
@@ -1,5 +1,9 @@
-import styled, {css} from "styled-components";
+import styled, {css, keyframes} from "styled-components";
 import Nav from '../common/navbar.js';
+
+const scale = keyframes`
+  0%{transform: scale(1, 1);}
+  100%{transform: scale(1.05, 1.05);}`;
 
 const Wrapper = styled.div`
   // Dimensiones de la caja
@@ -39,11 +43,12 @@ const Box = styled.div`
     grid-column: 2 / 3;
     grid-row: 1 / 6;
     display: grid;
-    grid-template-columns: repeat(21, 1fr);
-    grid-template-rows: repeat(18 , 1fr);
+    grid-template-columns: repeat(40, 1fr);
+    grid-template-rows: repeat(35 , 1fr);
   `}
 
   ${props => props.three && css`
+    border-radius: 0 0 2vw 2vw;
     background: #D9D9D9;
     color: black;
     //Posicionamiento por línea
@@ -78,9 +83,50 @@ const Image = styled.img`
   max-height: 100%;
   max-width: 100%`;
 
-const Cat_Frames = styled.a`
-  grid-column: 1;
-  grid-row: 1 / 9;`;
+//35 horizontal, 41 vertical
+
+const Cat_frames = styled.a`
+  border-radius: .2vh;
+  font-family: Times New Roman, "Trebuchet MS", Verdana, sans-serif;
+  &:hover {
+    animation: ${scale} 150ms cubic-bezier(0.1, 1.7, 1.0, 1.9) forwards;
+  }
+
+  
+  ${props => props.one && css`
+  background: red;
+  grid-column: 1 / 7;
+  grid-row: 1 / 7 ;`}
+
+  ${props => props.two && css`
+  background: blue;
+  grid-column: 7 / 27;
+  grid-row: 1 / 21;`}
+  
+  ${props => props.three && css`
+  background: yellow;
+  grid-column: 27 / 41;
+  grid-row: 1 / 14 ;`}
+  
+  ${props => props.four && css`
+  background: purple;
+  grid-column: 1 / 7;
+  grid-row: 7 / 21;`}
+  
+  ${props => props.five && css`
+  background: green;
+  grid-column: 27 / 41;
+  grid-row: 14 / 21;`}
+  
+  ${props => props.six && css`
+  background: pink;
+  grid-column: 1 / 20;
+  grid-row: 21 / 41;`}
+  
+  ${props => props.seven && css`
+  background: #FDC500;
+  grid-column: 20 / 41;
+  grid-row: 21 / 41;`};`
 
 function HView1() {
   return (
@@ -88,7 +134,7 @@ function HView1() {
       <Nav/>
       <Wrapper className="App" style={{}}>
           <Box><Image src={"https://images.unsplash.com/photo-1595257841889-eca2678454e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"}></Image></Box>
-          <Box two>Hola</Box>
+          <Box two><Cat_frames one/><Cat_frames two/><Cat_frames three/><Cat_frames four/><Cat_frames five/><Cat_frames six/><Cat_frames seven/></Box>
           <Box three wide>¿Qué comemos hoy?</Box>
       </Wrapper>
     </div>
