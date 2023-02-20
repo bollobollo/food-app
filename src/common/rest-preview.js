15ebaf591c9859276bb96e8aad52702d39ee5905
bollo <maardeva@gmail.com>
2023-02-20 16:59:06 -0600

    rest component added

--- src/common/rest-preview.js
+++ src/common/rest-preview.js
@@ -1,6 +1,10 @@
-import styled, {css} from "styled-components";
+import styled, {css, keyframes} from "styled-components";
 
-const Rest = styled.a`
+const scale = keyframes`
+  0%{transform: scale(1, 1);}
+  100%{transform: scale(1.05, 1.05);}`;
+
+const Box = styled.a`
     width: 30%;
     min-width: 200px;
     height: 31%;
@@ -9,8 +13,13 @@ const Rest = styled.a`
     background: white;
     border-radius: 1vw;
     cursor: pointer;
+    font-size: 1em;
+    padding: .3em;
     transition: width 500ms;
     transition-delay: 500ms;
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-template-rows: repeat(7 , 1fr);
 
     &:hover {
     width: 58%;
@@ -20,3 +29,20 @@ const Rest = styled.a`
     animation-delay: 500ms;
   }
     `;
+
+const Img = styled.img`
+    object-fit: cover;
+    grid-column: 1;
+    grid-row: 1 / 3 ;`;
+
+
+function Rest() {
+  return (
+    <Box>
+      <Img src="https://images.unsplash.com/photo-1576867757603-05b134ebc379?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+      Hola cómo estás?
+    </Box>
+  );
+}
+
+export default Rest;
