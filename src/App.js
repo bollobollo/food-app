ba3f47cec34ba60c09bc17ecbd67de138eaa0908
bollo <maardeva@gmail.com>
2023-03-02 16:47:54 -0600

    commit new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import HView1 from './home/home-view1.js';
-import HView2 from './home/home-view2.js';
+import Home1 from './home/home-view1.js';
+import Home2 from './home/home-view2.js';
+import Article1 from './article/article-view1';
 import styled, {css} from "styled-components";
 // import Ill from './home-illustration.js'
 
 function App() {
   return (
     <div style={{boxSizing: "content-box", background: "transparent", width: "100%"}}>
-      <HView1/>
-      <HView2/>
+      <Home1/>
+      <Home2/>
     </div>
   );
 }
